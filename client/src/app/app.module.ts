2bc4491e7c812f143ebae2d96334a3a8f9392a8e
Johannes Martinez <mart4695@morris.umn.edu>
2020-02-29 18:20:47 -0600

    Update app.module.ts to add imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import {MatButtonModule} from '@angular/material/button';
@@ -22,18 +22,16 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatRadioModule} from '@angular/material/radio';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import { UserListComponent } from './users/user-list.component';
-import { TodoListComponent } from './todos/todo-list.component';
 import { HomeComponent } from './home/home.component';
 import { UserService } from './users/user.service';
-import { TodoService } from './todos/todo.service';
 import { HttpClientModule } from '@angular/common/http';
 import { LayoutModule } from '@angular/cdk/layout';
 import { UserCardComponent } from './users/user-card.component';
 import { UserProfileComponent } from './users/user-profile.component';
-import { AddUserComponent } from './users/add-user.component';
+import { TodoListComponent } from './todos/todo-list.component';
+import { TodoService } from './todos/todo.service';
 
 const MATERIAL_MODULES: any[] = [
   MatListModule,
@@ -50,8 +48,7 @@ const MATERIAL_MODULES: any[] = [
   MatOptionModule,
   MatFormFieldModule,
   MatDividerModule,
-  MatRadioModule,
-  MatSnackBarModule
+  MatRadioModule
 ];
 
 @NgModule({
@@ -61,21 +58,21 @@ const MATERIAL_MODULES: any[] = [
     UserListComponent,
     UserCardComponent,
     UserProfileComponent,
-    AddUserComponent,
+    TodoListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    ReactiveFormsModule,
     FlexLayoutModule,
     HttpClientModule,
     MATERIAL_MODULES,
     LayoutModule,
   ],
   providers: [
-    UserService
+    UserService,
+    TodoService
   ],
   bootstrap: [AppComponent]
 })
